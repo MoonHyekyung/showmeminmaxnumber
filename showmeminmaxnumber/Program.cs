e0732d3b6cd970d2d75dfa176c6d42caf4f0685b
MoonHyekyung <moonsoft02@gmail.com>
2020-05-12 21:41:26 +0900

    for문과 입력문을 이용한 최대, 최소값 찾기

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,28 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int min, max;
+            
+            Console.Write("숫자를 입력해주세요 : ");
+            var num = int.Parse(Console.ReadLine());
+            min = num;
+            max = num;
+            for (int i = 0; i<4; i++)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                num = int.Parse(Console.ReadLine());
+                if(num<min)
+                {
+                    min = num;
+                }
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
         }
     }
 }
